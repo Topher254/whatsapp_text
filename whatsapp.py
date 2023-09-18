20d5d781b34bef41b553871ce3b0d27baeb86b4b
Topher254 <raphaelsarota@gmail.com>
2023-09-18 23:26:43 +0300

    datetime

--- whatsapp.py
+++ whatsapp.py
@@ -1,3 +1,9 @@
-print("hello")
+import pywhatkit
+import datetime
 
-numbers = [254796871876,254113349597]
+print(datetime.datetime.now())
+
+numbers = ["+254796871876","+254113349597"]
+
+for i in numbers:
+    pywhatkit.sendwhatmsg(i,"Hellow",23,26)
